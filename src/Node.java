ae0a9bbdb6fdd628e16c5f93ec584472ea75aa7e
dangchienhsgs <dangchienhsgs@gmail.com>
2014-06-11 12:29:02 +0700

    Update Node.java

--- src/Node.java
+++ src/Node.java
@@ -16,48 +16,72 @@ public class Node extends Canvas{
     Point StringPosition;
     int nodeHeight=50;
     int nodeWidth=50;
-    public void setNodeWidth(int nodeWidth){
-        this.nodeWidth=nodeWidth;
-    }
-    public void setLabel(String label){
-        this.label=label;
-        this.setToolTipText(label);
-    }
-    public void setTopLeft(int x, int y){
-        this.setLocation(x, y);
-        this.topLeft=this.getLocation();
-    }
+
+    /*Constructor */
     public Node(Composite parent, int style){
         super (parent, style);
     }
+
     public Node(Composite parent, int style, final String label) {
         super(parent, style);
         this.label = label;
     }
+
+    /* Set the node width */
+    public void setNodeWidth(int nodeWidth){
+        this.nodeWidth=nodeWidth;
+    }
+
+    /* Set the label */
+    public void setLabel(String label){
+        this.label=label;
+        this.setToolTipText("value: "+label);
+    }
+
+    /* set the topleft include set Location*/
+    public void setTopLeft(int x, int y){
+        this.setLocation(x, y);
+        this.setBounds(x, y, nodeWidth, nodeHeight);
+        this.topLeft=this.getLocation();
+    }
+
+    /* reset new color */
+    public void setColor(int background, int foreground){
+        this.setBackground(getDisplay().getSystemColor(background));
+        this.setForeground(getDisplay().getSystemColor(foreground));
+    }
+
+    /* get TopLeft corner */
     public Point getTopLeft(){
         return this.topLeft;
     }
+
+    /* get TopRight corner */
     public Point getTopRight(){
         return new Point(this.getTopLeft().x+nodeWidth, this.getTopLeft().y);
     }
+
+    /*get BellowLeft corner */
     public Point getBellowLeft(){
         return new Point(this.getTopLeft().x, this.getTopLeft().y+nodeHeight);
     }
+
+    /* get bellowRight corner */
     public Point getBellowRight(){
         return new Point(this.getTopLeft().x+nodeWidth, this.getTopLeft().y+nodeHeight);
     }
+
+    /* draw a Node */
     public void drawNode(){
         final Rectangle rec=this.getBounds();
         this.addPaintListener(new PaintListener() {
             @Override
             public void paintControl(PaintEvent e) {
-                ////e.gc.setBackground(getDisplay().getSystemColor(SWT.CO));
-                //e.gc.setForeground(getDisplay().getSystemColor(SWT.COLOR_GRAY));
-               // e.gc.fillGradientRectangle(0, 0, nodeWidth, nodeHeight, true);
-                //e.gc.setForeground(getDisplay().getSystemColor(SWT.COLOR_BLACK));
-                e.gc.drawString(label, 10, 10, true);
-                e.gc.drawRectangle(0, 0, nodeWidth, nodeHeight);
+                e.gc.drawString(label, 10, 15, true);
+                e.gc.drawRectangle(0, 0, nodeWidth-1, nodeHeight-1);
             }
         });
     }
-    }
+
+    /* transfrom */
+}
