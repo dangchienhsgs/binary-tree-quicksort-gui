1986b1663b165e18be29aa4028e201dacdcbd065
dangchienhsgs <dangchienhsgs@gmail.com>
2014-06-12 23:00:21 +0700

    Update GUI.java

--- src/GUI.java
+++ src/GUI.java
@@ -3,13 +3,20 @@ import org.eclipse.swt.events.*;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.*;
-
+/* The main of this program
+ *
+ */
 public class GUI {
     Display display;
     Shell shell;
     Node node;
     DrawingComposite drawPanel;
     Menu menuBar;
+
+
+    /* Constructor
+     *
+     */
     public GUI(){
         display=new Display();
         shell=new Shell(display);
@@ -17,12 +24,12 @@ public class GUI {
         shell.setText("Java Binary Tree Program");
         drawPanel=new DrawingComposite(this.shell, SWT.BORDER| SWT.V_SCROLL| SWT.H_SCROLL);
         drawPanel.setLayoutData(new GridData(GridData.FILL_VERTICAL | GridData.FILL_HORIZONTAL));
-        /*drawPanel.addNode(9);
-        drawPanel.addNode(3);
-        drawPanel.addNode(16);
-        drawPanel.addNode(14);
-        drawPanel.addNode(15);*/
+
     }
+
+    /* Open program
+     *
+     */
     public void open(){
         this.shell.open();
         this.shell.layout();
@@ -33,6 +40,10 @@ public class GUI {
             }
         }
     }
+
+    /* Create its menuBar
+     *
+     */
     public void createMenuBar(){
         menuBar=new Menu(shell, SWT.BAR);
 
@@ -48,118 +59,142 @@ public class GUI {
         actionMenuHeader.setMenu(actionMenu);
 
         MenuItem addNewNode=new MenuItem(editMenu, SWT.PUSH);
-        addNewNode.setText("Add new node");
+        addNewNode.setText("Create a new node");
         addNewNode.addSelectionListener(new SelectionAdapter() {
             @Override
             public void widgetSelected(SelectionEvent e) {
                 InputDialog numberInputDialog=new InputDialog(shell);
-                Integer value=numberInputDialog.open("Add a node by value", "Please give the value of node: ");
+                Integer value=numberInputDialog.open("Create a new node", "Input new node: ");
                 if (value!=null){
                     drawPanel.addNode(value);
                 }
 
             }
         });
-        ///////////////////////////////
+        //////////////////////////////////////////////////
 
         MenuItem browseNodeFirst=new MenuItem(actionMenu, SWT.PUSH);
-        browseNodeFirst.setText("Browse All Node First Type");
+        browseNodeFirst.setText("Browse (first type)");
         browseNodeFirst.addSelectionListener(new SelectionAdapter() {
             @Override
             public void widgetSelected(SelectionEvent selectionEvent) {
-                drawPanel.browseAllNode(drawPanel.tree.root, 1);
+                drawPanel.browseNode(drawPanel.tree.root, 1);
+                drawPanel.statusBar.setText("");
             }
         });
         //---------------------------------//
         MenuItem browseNodeMiddle=new MenuItem(actionMenu, SWT.PUSH);
-        browseNodeMiddle.setText("Browse All Node Middle Type");
+        browseNodeMiddle.setText("Browse (middle type)");
         browseNodeMiddle.addSelectionListener(new SelectionAdapter() {
             @Override
             public void widgetSelected(SelectionEvent selectionEvent) {
-                drawPanel.browseAllNode(drawPanel.tree.root, 2);
+                drawPanel.browseNode(drawPanel.tree.root, 2);
+                drawPanel.statusBar.setText("");
             }
         });
         //---------------------------------//
         MenuItem browseNodeLast=new MenuItem(actionMenu, SWT.PUSH);
-        browseNodeLast.setText("Browse All Node Last Type");
+        browseNodeLast.setText("Browse (last type)");
         browseNodeLast.addSelectionListener(new SelectionAdapter() {
             @Override
             public void widgetSelected(SelectionEvent selectionEvent) {
-                drawPanel.browseAllNode(drawPanel.tree.root, 3);
+                drawPanel.browseNode(drawPanel.tree.root, 3);
+                drawPanel.statusBar.setText("");
             }
         });
         ///////////////////////////////////////////////////////////
         MenuItem addNodeStep=new MenuItem(editMenu, SWT.PUSH);
-        addNodeStep.setText("Add node step");
+        addNodeStep.setText("Create a new node (Step)");
         addNodeStep.addSelectionListener(new SelectionAdapter() {
                     @Override
                     public void widgetSelected(SelectionEvent e) {
                         InputDialog inputDialog = new InputDialog(shell);
-                        Integer value = inputDialog.open("Add a node by value", "Please give the value of node: ");
+                        Integer value = inputDialog.open("Create a new node (Step)", "Input new node: ");
                         if (value != null) {
                             drawPanel.addNodeStep(drawPanel.tree.root, value);
+                            drawPanel.statusBar.setText("");
                         }
                     }
                 });
         MenuItem findNode=new MenuItem(actionMenu, SWT.PUSH);
-        findNode.setText("Find a node");
+        findNode.setText("Find");
         findNode.addSelectionListener(new SelectionAdapter() {
             @Override
             public void widgetSelected(SelectionEvent e) {
                 InputDialog inputDialog=new InputDialog(shell);
-                Integer value=inputDialog.open("Find node by value", "Please give the value of node: ");
+                Integer value=inputDialog.open("Find", "Input value: ");
                 if (value!=null){
                     drawPanel.findNodeStep(drawPanel.tree.root, value);
+                    drawPanel.statusBar.setText("");
                 }
             }
         });
         ///////////////////////////
         MenuItem removeNode=new MenuItem(editMenu, SWT.PUSH);
-        removeNode.setText("Remove a node by value");
+        removeNode.setText("Remove");
         removeNode.addSelectionListener(new SelectionAdapter() {
             @Override
             public void widgetSelected(SelectionEvent e) {
                 InputDialog inputDialog=new InputDialog(shell);
-                Integer value=inputDialog.open("Delete a node", "Please give the value of the node: ");
+                Integer value=inputDialog.open("Remove", "Input value: ");
                 if (value!=null){
                     drawPanel.treeNode =drawPanel.tree.root;
                     drawPanel.remove_findNode(value);
+                    drawPanel.statusBar.setText("");
                 }
             }
         });
         shell.setMenuBar(menuBar);
         /////////////////////////////
         MenuItem findSuccessor=new MenuItem(actionMenu, SWT.PUSH);
-        findSuccessor.setText("Find a succesor of a node");
+        findSuccessor.setText("Get successor");
         findSuccessor.addSelectionListener(new SelectionAdapter() {
             @Override
             public void widgetSelected(SelectionEvent selectionEvent) {
                 InputDialog inputDialog=new InputDialog(shell);
-                Integer value=inputDialog.open("Delete a node", "Please give the value of the node: ");
+                Integer value=inputDialog.open("Get successor of a node", "Input value: ");
                 if (value!=null){
                     drawPanel.findSucesscor(value);
+                    drawPanel.statusBar.setText("");
                 }
             }
         });
         ///////////////////////////////
         MenuItem findPredecessor=new MenuItem(actionMenu, SWT.PUSH);
-        findPredecessor.setText("Find a predesesscor of a node");
+        findPredecessor.setText("Get predecessor");
         findPredecessor.addSelectionListener(new SelectionAdapter() {
             @Override
             public void widgetSelected(SelectionEvent selectionEvent) {
                 InputDialog inputDialog=new InputDialog(shell);
-                Integer value=inputDialog.open("Delete a node", "Please give the value of the node: ");
+                Integer value=inputDialog.open("Get predecessor", "Input value: ");
                 if (value!=null){
-                    drawPanel.findPredesscor(value);
+                    drawPanel.findPredecessor(value);
+                    drawPanel.statusBar.setText("");
                 }
             }
         });
+        MenuItem findMax=new MenuItem(actionMenu, SWT.PUSH);
+        findMax.setText("Find Max Node");
+        findMax.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent selectionEvent) {
+                drawPanel.findMaxNode(drawPanel.tree.root);
+            }
+        });
+        MenuItem findMin=new MenuItem(actionMenu, SWT.PUSH);
+        findMin.setText("Find Min Node");
+        findMin.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent selectionEvent) {
+                drawPanel.findMinNode(drawPanel.tree.root);
+            }
+        });
         final MenuItem heapSort=new MenuItem(actionMenu, SWT.PUSH);
-        heapSort.setText("HeapSort");
+        heapSort.setText("Heap Sort Algorithm");
         heapSort.addSelectionListener(new SelectionAdapter() {
             @Override
             public void widgetSelected(SelectionEvent selectionEvent) {
-                int value[]=new InputArray(shell).open("HeapSort", "Give me the array: ");
+                int value[]=new InputArray(shell).open("Heap Sort Algorithm", "Input an integer array: ");
                 if (value!=null){
 //                    Display display = new Display();
                     Shell shell2 = new Shell(display);
@@ -182,18 +217,23 @@ public class GUI {
                         }
                     }
                 }
+                drawPanel.statusBar.setText("");
             }
         });
         /////////////////////////////
         MenuItem printItem=new MenuItem(actionMenu, SWT.PUSH);
-        printItem.setText("Give the list sort elements");
+        printItem.setText("Get sorted list");
         printItem.addSelectionListener(new SelectionAdapter() {
             @Override
             public void widgetSelected(SelectionEvent selectionEvent) {
                 drawPanel.printElement();
+                drawPanel.statusBar.setText("");
             }
         });
     }
+    /*
+     * Main
+     */
     public static void main(String args[]){
         GUI gui=new GUI();
         gui.createMenuBar();
